0525b599c65920ae7d01cc3437a5bf7ddbdfb523
Jong1 <44349716+donsonioc2010@users.noreply.github.com>
2023-06-03 14:14:19 +0900

    [unrated] Title: 조건에 맞게 수열 변환하기 1, Time: 6.32 ms, Memory: 111 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181882. 조건에 맞게 수열 변환하기 1/조건에 맞게 수열 변환하기 1.java
@@ -0,0 +1,13 @@
+import java.util.Arrays;
+
+class Solution {
+  public int[] solution(int[] arr) {
+    return Arrays.stream(arr).map(num -> {
+      if((num >= 50) && (num % 2 == 0)) 
+        return num / 2;
+      else if(num <= 50 && num % 2 == 1) 
+        return num * 2;
+      return num;
+    }).toArray();
+  }
+}
