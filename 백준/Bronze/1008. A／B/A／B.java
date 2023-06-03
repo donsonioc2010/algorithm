1b2dc13a3d57d28651cd65f70fcdad0f2405f343
Jong1 <44349716+donsonioc2010@users.noreply.github.com>
2023-06-03 13:02:59 +0900

    [Bronze V] Title: A/B, Time: 132 ms, Memory: 15988 KB -BaekjoonHub

---
+++ 백준/Bronze/1008. A／B/A／B.java
@@ -0,0 +1,14 @@
+import java.util.*;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Scanner sc  = new Scanner(System.in);
+		double a,b;
+        a = sc.nextDouble();
+        b = sc.nextDouble();
+		double c = a/b;
+		System.out.println(c);
+	}
+
+}
