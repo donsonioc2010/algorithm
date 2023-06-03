6a6d3293b828a284d1d04ea7612609ea5790ba43
Jong1 <44349716+donsonioc2010@users.noreply.github.com>
2023-06-03 12:10:59 +0900

    [Bronze V] Title: 개, Time: 116 ms, Memory: 13876 KB -BaekjoonHub

---
+++ 백준/Bronze/10172. 개/개.java
@@ -0,0 +1,9 @@
+public class Main {
+    
+  public static void main(String[] args) {
+    StringBuffer sb = new StringBuffer();
+    sb.append("|\\_/|\n").append("|q p|   /}\n").append("( 0 )\"\"\"\\\n")
+        .append("|\"^\"`    |\n").append("||_/=\\\\__|");
+    System.out.println(sb.toString());
+  }
+}
