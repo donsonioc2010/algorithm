7bc3c16462d39c4ec2c728b547ae322b6de3f4cc
Jong1 <44349716+donsonioc2010@users.noreply.github.com>
2023-06-04 14:42:29 +0900

    [unrated] Title: 접미사 배열, Time: 2.31 ms, Memory: 83.6 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181909. 접미사 배열/접미사 배열.java
@@ -0,0 +1,8 @@
+import java.util.stream.IntStream;
+
+class Solution {
+  public String[] solution(String my_string) {
+    return IntStream.range(0, my_string.length()).mapToObj(i -> (String) my_string.substring(i)).sorted()
+        .toArray(String[]::new);
+  }
+}
