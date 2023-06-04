9972f04d0e2c688e702baa0791ef1e669063a885
Jong1 <44349716+donsonioc2010@users.noreply.github.com>
2023-06-04 14:16:01 +0900

    [unrated] Title: 접미사인지 확인하기, Time: 0.02 ms, Memory: 72.3 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181908. 접미사인지 확인하기/접미사인지 확인하기.java
@@ -0,0 +1,8 @@
+class Solution {
+  public int solution(String my_string, String is_suffix) {
+    if (my_string.length() == is_suffix.length() || my_string.length() < is_suffix.length()) {
+      return my_string.equals(is_suffix) ? 1 : 0;
+    }
+    return my_string.substring(my_string.length() - is_suffix.length()).equals(is_suffix) ? 1 : 0;
+  }
+}
