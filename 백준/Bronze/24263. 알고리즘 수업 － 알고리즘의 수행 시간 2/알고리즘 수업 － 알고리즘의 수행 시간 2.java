fedc025331dd7953bf2cc53d0338d0cb36a4fb07
Jong1 <44349716+donsonioc2010@users.noreply.github.com>
2023-06-09 17:40:09 +0900

    [Bronze IV] Title: 알고리즘 수업 - 알고리즘의 수행 시간 2, Time: 128 ms, Memory: 14160 KB -BaekjoonHub

---
+++ 백준/Bronze/24263. 알고리즘 수업 － 알고리즘의 수행 시간 2/알고리즘 수업 － 알고리즘의 수행 시간 2.java
@@ -0,0 +1,14 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+  public static void main(String[] args) {
+    try (BufferedReader br = new BufferedReader(new InputStreamReader(System.in))) {
+      System.out.println(br.readLine());
+      System.out.println(1);
+    } catch (IOException e) {
+      e.printStackTrace();
+    }
+  }
+}
