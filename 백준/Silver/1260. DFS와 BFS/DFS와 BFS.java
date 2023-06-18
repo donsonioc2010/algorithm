1882517a11305e1a3152e9e0f2ba973442bb655b
Jong1 <44349716+donsonioc2010@users.noreply.github.com>
2023-06-18 21:38:38 +0900

    [Silver II] Title: DFS와 BFS, Time: 364 ms, Memory: 23280 KB -BaekjoonHub

--- 백준/Silver/1260. DFS와 BFS/DFS와 BFS.java
+++ 백준/Silver/1260. DFS와 BFS/DFS와 BFS.java
@@ -1,131 +1,105 @@
-import java.util.Arrays;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.HashMap;
 import java.util.LinkedList;
-import java.util.Scanner;
+import java.util.List;
+import java.util.Map;
 
 public class Main {
 
-	public static void main(String[] args) {
-		/* [선언부] */
-        int nodeNum;        // N
-        int m;              // M
-        int v;              // V
-        int[][] mapAry;     // LinkedList대신 사용할 배열
-        Scanner sc = new Scanner(System.in);
+  public static void main(String[] args) throws Exception {
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    int N, M, V;
+    Map<Integer, List<Integer>> fsMap = new HashMap<>();
 
-        /* N, M, V INPUT*/
-        nodeNum = sc.nextInt();
-        m       = sc.nextInt();
-        v       = sc.nextInt();
+    // 초기화
+    String[] NMV = br.readLine().split(" ");
+    N = Integer.parseInt(NMV[0]);
+    M = Integer.parseInt(NMV[1]);
+    V = Integer.parseInt(NMV[2]);
 
+    for (int i = 0; i <= N; i++) {
+      fsMap.put(i, new ArrayList<>());
+    }
 
-        /* Validation */
-        if((nodeNum < 1 && nodeNum > 1000) && (m < 1 && m > 10000)) {
-            return;
-        }
-
-        /* DFS Class, BFS Class 둘다 사용하기 위한 LinkedList 생성 */
-        mapAry = new int[nodeNum + 1][nodeNum + 1]; // 증가시키는건 딱 그숫자에 맞춰서 사용을 하기 위함
-
-        /* 배열 초기화 */
-        for(int i = 0; i <= nodeNum; i++) {
-            Arrays.fill(mapAry[i], 0);
-        }
-
-        /* DFS, BFS 에 사용될 맵제작 */
-        for(int i=1; i <= m; i++){
-            int to      = sc.nextInt();
-            int from    = sc.nextInt();
-            mapAry[to][from] = mapAry[from][to] = 1;
-        }
-
-
-        // DFS실행
-        new dfs_retry(nodeNum, v, mapAry);
-
-        // BFS실행
-        new bfs_retry(nodeNum, v, mapAry);
-	}
+    for (int i = 0; i < M; i++) {
+      String[] line = br.readLine().split(" ");
+      int stNum = Integer.parseInt(line[0]);
+      int edNum = Integer.parseInt(line[1]);
+      fsMap.get(stNum).add(edNum); // Map의 List에 넣는법
+      fsMap.get(edNum).add(stNum);
+    }
+    new dfs(N, V, fsMap);
+    new bfs(N, V, fsMap);
+  }
 }
 
-/* 깊이 완전 탐색 */
-class dfs_retry {
-    private int[] visited;              // 방문시에 해당방에 값을 늘리는방식
-    private int nodeNum;
-    private StringBuilder sb = new StringBuilder();
+class dfs {
+  private int[] visited;
+  private Map<Integer, List<Integer>> fsMap = new HashMap<>();
+  private int N;
+  private StringBuffer sb = new StringBuffer();
 
+  public dfs(int N, int V, Map<Integer, List<Integer>> map) {
+    this.fsMap = map;
+    this.N = N;
+    visited = new int[N + 1];
+    run(V);
+    System.out.println(sb);
+  }
 
-    /* DFS실행 */
-    public dfs_retry(int nodeNum, int stNum, int[][] mapAry) {  // 배열크기, 시작번호, main에서 제작된 리스트
-        visited = new int[nodeNum + 1]; // 해당 노드를 방문했었는지 안했었는지를 판별하기 위함.
-        Arrays.fill(visited, 0);
+  private void run(int V) {
+    visited[V]++;
+    sb.append(V + " ");
 
-        this.nodeNum = nodeNum;
+    List<Integer> nodeList = fsMap.get(V);
+    nodeList.sort(Comparator.naturalOrder());
 
-        dfsDoRun(stNum, mapAry);
-        System.out.println(sb.toString());
+    for (int i = 0; i < nodeList.size(); i++) {
+      int nodeNum = nodeList.get(i);
+      if (visited[nodeNum] == 0) {
+        run(nodeNum);
+      }
     }
+  }
 
-    /* 탐색 실행 */
-    private void dfsDoRun(int num, int[][] mapAry) {
-        visited[num] += 1; // 값을 증가해서 방문구분
-        dfsPrintStack(num);
-
-        // 시작번호 => 맵이 존재하고 방문안했다면 해당번호로 재귀
-        for(int i = 0 ; i <= nodeNum; i++) {
-            if(mapAry[num][i] == 1 && visited[i] == 0) {
-                dfsDoRun(i, mapAry);
-            }
-        }
-    }
-
-    /* 출력을 진행할 스택을 쌓음 */
-    private void dfsPrintStack(int printNum) {
-        sb.append(printNum+" ");
-        //System.out.println(printNum + " ");
-    }
 }
 
-/* 너비 우선 탐색 */
-class bfs_retry {
-    private int[] visited;
-    private int nodeNum;
+class bfs {
+  private int[] visited;
+  private Map<Integer, List<Integer>> fsMap = new HashMap<>();
+  private LinkedList<Integer> list = new LinkedList<>();
+  private int N;
+  private StringBuffer sb = new StringBuffer();
 
-    private StringBuilder sb = new StringBuilder();
+  public bfs(int N, int V, Map<Integer, List<Integer>> map) {
+    this.fsMap = map;
+    this.N = N;
+    visited = new int[N + 1];
+    run(V);
+    System.out.println(sb);
+  }
 
+  private void run(int V) {
+    list.add(V);
+    int num;
+    while (list.size() > 0) {
+      num = list.poll();
+      visited[num]++;
+      if (visited[num] == 1) {
+        sb.append(num + " ");
+      }
 
-    public bfs_retry(int nodeNum, int stNum, int[][] mapAry) {
-        visited = new int[nodeNum+1];
-
-        this.nodeNum = nodeNum;
-
-        bfsDoRun(stNum, mapAry);
-        System.out.println(sb.toString());
-    }
-
-    /* bfs실행 */
-    private void bfsDoRun(int num, int[][] mapAry) {
-        // bfs 리스트제작할 방
-        LinkedList<Integer> list = new LinkedList<Integer>();
-        list.add(num);
-
-        while(list.size() > 0) {
-            //list에 방문이 안되면 계속 list가 추가되면서 추가된 리스트를 바로 당겨오고 프린트스택에 넣음.
-            num = list.poll();
-            visited[num] += 1;      //방문 체크
-            bfsPrintStack(num);
-
-            for(int i = 0; i <= nodeNum; i++) {
-                if(mapAry[num][i] == 1 && visited[i] == 0 ) {
-                    visited[i] += 1;
-                    list.add(i);
-                }
-            }
+      List<Integer> tempList = fsMap.get(num);
+      tempList.sort(Comparator.naturalOrder());
+      for (int i = 0; i < tempList.size(); i++) {
+        if (visited[tempList.get(i)] == 0) {
+          list.add(tempList.get(i));
         }
+      }
     }
 
-    /* 출력을 진행할 스택을 쌓음 */
-    private void bfsPrintStack(int printNum) {
-        sb.append(printNum+" ");
-        //System.out.println(printNum + " ");
-    }
+  }
 }
