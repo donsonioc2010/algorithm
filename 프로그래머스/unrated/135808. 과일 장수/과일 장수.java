47845c36716e1258c3ade3ac4f8e4e0ca13c9ff1
Jong1 <44349716+donsonioc2010@users.noreply.github.com>
2023-06-22 10:52:44 +0900

    [unrated] Title: 과일 장수, Time: 5.18 ms, Memory: 71.8 MB -BaekjoonHub

--- 프로그래머스/unrated/135808. 과일 장수/과일 장수.java
+++ 프로그래머스/unrated/135808. 과일 장수/과일 장수.java
@@ -1,15 +1,6 @@
-import java.util.Arrays;
-import java.util.Comparator;
-
 class Solution {
-  public int solution(int k, int m, int[] score) {
-    int listCnt = score.length / m;
-    score = Arrays.stream(score).boxed().sorted(Comparator.reverseOrder()).mapToInt(i -> i).toArray();
-    int answer = 0;
-
-    for (int i = 0; i < listCnt; i++) 
-      answer += Arrays.stream(Arrays.copyOfRange(score, (i * m), ((i * m) + m))).min().orElse(-1) * m;
-    
-    return answer;
-  }
+    public int solution(int k, int m, int[] score) {
+        int answer = 0;
+        return answer;
+    }
 }
