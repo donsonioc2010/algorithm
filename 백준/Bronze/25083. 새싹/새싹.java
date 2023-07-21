7790d23e1934a911a5fab368f8f11a5d82b55f53
Jong1 <44349716+donsonioc2010@users.noreply.github.com>
2023-07-21 16:12:56 +0900

    [Bronze V] Title: 새싹, Time: 116 ms, Memory: 13964 KB -BaekjoonHub

---
+++ 백준/Bronze/25083. 새싹/새싹.java
@@ -0,0 +1,16 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class Main {
+  private static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+  public static void main(String[] args) throws Exception {
+    StringBuilder sb = new StringBuilder("         ,r'\"7\n")
+        .append("r`-_   ,'  ,/\n")
+        .append(" \\. \". L_r'\n")
+        .append("   `~\\/\n")
+        .append("      |\n")
+        .append("      |\n");
+    System.out.println(sb);
+  }
+}
